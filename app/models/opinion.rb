beb455d9cb5911d2332fb7e74de4a3cd56eb4822
Barack <barackmukelenga100@gmail.com>
2021-06-17 13:15:35 +0200

    Fix rubocop errors

--- app/models/opinion.rb
+++ app/models/opinion.rb
@@ -1,5 +1,5 @@
 class Opinion < ApplicationRecord
-    validates :text, presence: true, length: {minimun: 3, maximum: 250}
+  validates :text, presence: true, length: { minimun: 3, maximum: 250 }
 
-    belongs_to :author, class_name: 'User'
+  belongs_to :author, class_name: 'User'
 end
