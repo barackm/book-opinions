373f07bf76f09836521d004828301b7f53ea052c
Barack <barackmukelenga100@gmail.com>
2021-06-20 20:28:07 +0200

    Add all the necessary fonctionality for creating comment on particular opinions

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,21 @@
 class CommentsController < ApplicationController
+    before_action :authenticate_user
+
+    def create 
+        opinion = Opinion.find(comment_params[:opinion_id])
+        comment = opinion.comments.build(user_id: current_user.id, content: comment_params[:content])
+        if comment.save 
+            flash[:notice] = "Welcome to Book Reviewer ✨" 
+            redirect_to root_path
+        else
+            flash[:alert] = "Oooh, The comment couldin't be saved. 😥"
+            redirect_to new_session_path
+        end
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :opinion_id)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "opinions#index"
   resources :users
-  resources :opinions
+  resources :opinions do 
+  end
   resources :sessions, only: [:new, :create, :destroy]
+  resources :comments, only: [:create]
   resources :followings, only: [:create, :destroy]
 end
