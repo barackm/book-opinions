59fe9d9e826c11526512304a33ce2148545b5276
Barack <barackmukelenga100@gmail.com>
2021-06-22 10:07:17 +0200

    Add a a proper redirect link path after canceling the a user account

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -55,7 +55,7 @@ class UsersController < ApplicationController
     @user.destroy
 
     flash[:notice] = 'Account canceled successfully.✨'
-    redirect_to root_path
+    redirect_to new_session_path
   end
 
   private
